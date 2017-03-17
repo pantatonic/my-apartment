a7e70814f024bf6f7b5e95b2ff8dc10f06f65ce1
sittha <sittha_a@hotmail.com>
2017-03-17 16:11:36 +0700

    update database

--- my-apartment/src/main/webapp/WEB-INF/db.sql
+++ my-apartment/src/main/webapp/WEB-INF/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 04, 2017 at 07:40 AM
+-- Generation Time: Mar 17, 2017 at 10:10 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -13,6 +13,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `my_apartment`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `building`
+--
+
+CREATE TABLE `building` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` text,
+  `tel` varchar(255) DEFAULT NULL,
+  `electricity_charge_per_unit` decimal(20,2) NOT NULL,
+  `min_electricity_unit` int(11) DEFAULT NULL,
+  `min_electricity_charge` decimal(20,2) DEFAULT NULL,
+  `water_charge_per_unit` decimal(20,2) NOT NULL,
+  `min_water_unit` int(11) DEFAULT NULL,
+  `min_water_charge` decimal(20,2) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -34,9 +53,38 @@ INSERT INTO `test` (`id`, `name`, `surname`) VALUES
 (1, 'Somchai', 'Jaidee'),
 (2, 'Somsri', 'Jaiyen');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `is_admin` int(1) NOT NULL DEFAULT '0',
+  `status` int(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `is_admin`, `status`) VALUES
+(1, 'System', 'Admin', 'admin@admin.com', '1234', 1, 1);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `building`
+--
+ALTER TABLE `building`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `test`
@@ -45,11 +93,27 @@ ALTER TABLE `test`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `building`
+--
+ALTER TABLE `building`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `test`
 --
 ALTER TABLE `test`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
