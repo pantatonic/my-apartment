2f4405688f4a3525fae9987b90f83f75e27b4c5a
sittha <sittha_a@hotmail.com>
2017-03-22 15:53:07 +0700

    ทำรวบการ Insert กับ Update ของ Building Service

--- my-apartment-services/src/main/java/my/apartment/services/BuildingDaoImpl.java
+++ my-apartment-services/src/main/java/my/apartment/services/BuildingDaoImpl.java
@@ -11,22 +11,25 @@ import java.util.logging.Logger;
 import my.apartment.common.Config;
 import my.apartment.model.Building;
 
-
 public class BuildingDaoImpl implements BuildingDao {
-    
+
     public Building save(Building building) {
         Connection con = null;
         PreparedStatement ps = null;
-        
+
         Building buildingReturn = null;
-        
+
         try {
             Class.forName(Config.JDBC_DRIVER);
-            
+
             con = DriverManager.getConnection(Config.DB_URL, Config.DB_USER, Config.DB_PASSWORD);
+
+            String querysString = "";
             
-            if(building.getId() == null) {
-                String querysString = "INSERT INTO building ("
+            if (building.getId() == null) {
+                /** insert process */
+                
+                querysString = "INSERT INTO building ("
                         + "name, "
                         + "address, "
                         + "tel, "
@@ -34,46 +37,75 @@ public class BuildingDaoImpl implements BuildingDao {
                         + "min_electricity_unit, "
                         + "min_electricity_charge, "
                         + "water_charge_per_unit, "
-                        + "min_water_unit, min_water_charge) "
+                        + "min_water_unit, "
+                        + "min_water_charge) "
                         + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-                ps = con.prepareStatement(querysString, Statement.RETURN_GENERATED_KEYS);
-                ps.setString(1, building.getName());
-                ps.setString(2, building.getAddress());
-                ps.setString(3, building.getTel());
-                ps.setBigDecimal(4, building.getElectricityChargePerUnit());
-                ps.setInt(5, building.getMinElectricityUnit());
-                ps.setBigDecimal(6, building.getMinElectricityCharge());
-                ps.setBigDecimal(7, building.getWaterChargePerUnit());
-                ps.setInt(8, building.getMinWaterUnit());
-                ps.setBigDecimal(9, building.getMinWaterCharge());
+            } else {
+                /** update process */
                 
-                Integer effectRow = ps.executeUpdate();
-                if(effectRow != 0) {
-                    ResultSet generatedKeys = ps.getGeneratedKeys();
+                querysString = "UPDATE building SET "
+                    + "name = ?, "
+                    + "address = ?, "
+                    + "tel = ?, "
+                    + "electricity_charge_per_unit = ?, "
+                    + "min_electricity_unit = ?, "
+                    + "min_electricity_charge = ?, "
+                    + "water_charge_per_unit = ?, "
+                    + "min_water_unit = ?, "
+                    + "min_water_charge = ? "
+                    + "WHERE id = ?";
+            }
+            
+
+            ps = con.prepareStatement(querysString, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, building.getName());
+            ps.setString(2, building.getAddress());
+            ps.setString(3, building.getTel());
+            ps.setBigDecimal(4, building.getElectricityChargePerUnit());
+            ps.setInt(5, building.getMinElectricityUnit());
+            ps.setBigDecimal(6, building.getMinElectricityCharge());
+            ps.setBigDecimal(7, building.getWaterChargePerUnit());
+            ps.setInt(8, building.getMinWaterUnit());
+            ps.setBigDecimal(9, building.getMinWaterCharge());
+            
+            if (building.getId() != null) {
+                /** insert process */
+                
+                ps.setInt(10, building.getId());
+            }
+
+            Integer effectRow = ps.executeUpdate();
+            
+            if (effectRow != 0) {
+                ResultSet generatedKeys = ps.getGeneratedKeys();
+                buildingReturn = new Building();
+                
+                if (building.getId() == null) {
+                    /** insert process */
+                    
                     if (generatedKeys.next()) {
-                        buildingReturn = new Building();
                         buildingReturn.setId(generatedKeys.getInt(1));
                     }
                 }
+                else {
+                    /** update process */
+                    
+                    buildingReturn.setId(building.getId());
+                }
             }
-            else {
-                System.out.println("UPDATE");
-            }
-        }
-        catch(Exception e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            if(ps != null) {
+        } finally {
+            if (ps != null) {
                 try {
                     ps.close();
                 } catch (SQLException ex) {
                     Logger.getLogger(LoginDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-            
-            if(con != null) {
+
+            if (con != null) {
                 try {
                     con.close();
                 } catch (SQLException ex) {
@@ -81,9 +113,7 @@ public class BuildingDaoImpl implements BuildingDao {
                 }
             }
         }
-        
-        
-        
+
         return buildingReturn;
     }
 }
